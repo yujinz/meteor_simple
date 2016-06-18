98af0389931895a146d1e16d508f81b6e12e8f11
yujinz <yujinz@umich.edu>
2016-06-18 13:03:00 -0400

    delete button

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,40 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js';
 
+import './task.js';
 import './body.html';
+
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
 
 Template.body.helpers({
   tasks() {
-    return Tasks.find({});
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.text.value = '';
+  },
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   },
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,20 @@
 import { Meteor } from 'meteor/meteor';
+import { Tasks } from '/imports/api/tasks.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+/*
+import { Tasks } from '../imports/api/tasks.js';
+
+    Meteor.startup(function(){
+      if(Tasks.find().count() == 0){
+        Tasks.insert({serverInsert:"test from server"},function(err,result){
+         if(!err){
+          console.log("insert succefull")
+        }
+     })
+    }
+  })
+*/
